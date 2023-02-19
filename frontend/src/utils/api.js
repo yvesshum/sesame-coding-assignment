f1675a8a7df98f5ef0dba0cad691ed05170a85fe
Yves Shum <yvesshum1210@gmail.com>
2023-02-19 04:08:25 -0800

    changed alert to toast

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {store} from "../app/store";
 import { authenticateWithPublicKey } from "../features/User/userSlice";
 import config from "../config.json"
+import { toast } from "react-toastify";
 const api = axios.create({
     baseURL:
         process.env.NODE_ENV === "development"
@@ -16,7 +17,7 @@ api.interceptors.response.use(
     },
     (error) => {
         if (error.name === "AxiosError" && error.response.status === 401) {
-            window.alert("Please verify your identity to continue");
+            toast.info("Please verify your identity to continue")
             store.dispatch(authenticateWithPublicKey(store.getState().user.publicKey))
         }
         return Promise.reject(error);
